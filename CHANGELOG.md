ca0ade51544cc7e7a861305121d4f0f659d53be2
Jacob Phillips <jifalops@gmail.com>
2018-06-13 22:20:12 -0400

    Add example usage

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.0.2] - June 13, 2018
+
+* Added example usage.
+
 ## [0.0.1] - June 13, 2018
 
 * First release.
---
+++ example/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../lib/network_resource.dart';
+
+final _resource1 = NetworkResource(
+  url: 'http://example.com/resource1.json',
+  filename: 'resource1.json',
+  maxAge: Duration(minutes: 60),
+);
+
+class MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: FutureBuilder<ResourceData>(
+      future: _resource1.get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Text('${snapshot.data.data}');
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+        return Center(child: CircularProgressIndicator());
+      },
+    ));
+  }
+}
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Network Resource example',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  MyHomePageState createState() {
+    return new MyHomePageState();
+  }
+}
